fixed findstats for image size issue, phase in radians, adjusted dataset

train_sep.py
@@ -9,7 +9,7 @@ import time
 '''
     don't forget about iterator.dataset.std = number to set some noise 
 '''
-datatype = 'phase'
+datatype = 'amp'
 
 
 def train():
@@ -44,7 +44,7 @@ def train():
 
     lossfn = torch.nn.MSELoss()
 
-    iterator_train = GenerateIterator_train(args, './data/arrays/train', eval=False, datatype=datatype)
+    iterator_train = GenerateIterator_train(args, './data/arrays/train', datatype=datatype)
     iterator_val = GenerateIterator_val(args, './data/arrays/val', datatype=datatype)
 
     if torch.cuda.is_available():
@@ -77,7 +77,7 @@ def train():
             loss_sum += loss.item()
             batch_num += 1
 
-            progress_bar.set_description('Loss: {:.5f} '.format(loss_sum / batch_num + 1e-6))
+            progress_bar.set_description('Loss: {:.5f} '.format(loss_sum / (batch_num + 1e-6)))
 
         if epoch % 1 == 0:
             with torch.no_grad():
@@ -101,7 +101,7 @@ def train():
                 preds = np.asarray(preds)
                 gts = np.asarray(gts)
 
-                val_f1_score = (np.sum(np.abs(preds.flatten() - gts.flatten())) / len(gts))
+                val_f1_score = (np.sum(np.abs(preds.flatten() - gts.flatten())) / len(gts.flatten()))
 
             print('|| Ep {} || Secs {:.1f} || Loss {:.1f} || Val f1 score {:.3f} || Val Loss {:.3f} ||\n'.format(
                     epoch,

utils/dataset.py
@@ -6,14 +6,19 @@ import random
 import os
 import torch
 from myargs import args
-import math
 from utils.UV_plane_generator import uv_generate
 
-AMP_MEAN = (0.500,)
-AMP_STD = (0.500,)
+TRAIN_AMP_MEAN = (16.9907,)
+TRAIN_AMP_STD = (157.3094,)
 
-PHASE_MEAN = (0.,)
-PHASE_STD = (2*math.pi,)
+TRAIN_PHASE_MEAN = (0.,)
+TRAIN_PHASE_STD = (1.8141,)
+
+VAL_AMP_MEAN = (0.00221,)
+VAL_AMP_STD = (0.03018,)
+
+VAL_PHASE_MEAN = (0.,)
+VAL_PHASE_STD = (1.8104,)
 
 eps = 1e-8
 
@@ -201,7 +206,7 @@ class Dataset_train(data.Dataset):
         return image, label
 
 
-def GenerateIterator_train(args, impath, eval=False, shuffle=True, datatype='amp'):
+def GenerateIterator_train(args, impath, shuffle=True, datatype='amp'):
     params = {
         'batch_size': args.batchSize,
         'shuffle': shuffle,
@@ -210,7 +215,7 @@ def GenerateIterator_train(args, impath, eval=False, shuffle=True, datatype='amp
         'drop_last': False,
     }
 
-    return data.DataLoader(Dataset_train(impath, eval=eval, datatype=datatype), **params)
+    return data.DataLoader(Dataset_train(impath, eval=False, datatype=datatype), **params)
 
 
 def normalizepatch(p, gt, eval, std, datatype):
@@ -229,9 +234,18 @@ def normalizepatch(p, gt, eval, std, datatype):
     p = torch.from_numpy(p).unsqueeze(0)
     gt = torch.from_numpy(gt).unsqueeze(0)
 
-    if datatype == 'amp':
-        return transforms.Normalize(mean=AMP_MEAN, std=AMP_STD)(p.float()), gt.float()
+    if datatype == 'amp' and eval:
+        return transforms.Normalize(mean=VAL_AMP_MEAN, std=VAL_AMP_STD)(p.float()), \
+            transforms.Normalize(mean=VAL_AMP_MEAN, std=VAL_AMP_STD)(gt.float())
 
-    elif datatype == 'phase':
-        return transforms.Normalize(mean=PHASE_MEAN, std=PHASE_STD)(p.float()), \
-            transforms.Normalize(mean=PHASE_MEAN, std=PHASE_STD)(gt.float())
+    elif datatype == 'phase' and eval:
+        return transforms.Normalize(mean=VAL_PHASE_MEAN, std=VAL_PHASE_STD)(p.float()), \
+            transforms.Normalize(mean=VAL_PHASE_MEAN, std=VAL_PHASE_STD)(gt.float())
+
+    elif datatype == 'amp' and not eval:
+        return transforms.Normalize(mean=TRAIN_AMP_MEAN, std=TRAIN_AMP_STD)(p.float()), \
+            transforms.Normalize(mean=TRAIN_AMP_MEAN, std=TRAIN_AMP_STD)(gt.float())
+
+    elif datatype == 'phase' and not eval:
+        return transforms.Normalize(mean=TRAIN_PHASE_MEAN, std=TRAIN_PHASE_STD)(p.float()), \
+            transforms.Normalize(mean=TRAIN_PHASE_MEAN, std=TRAIN_PHASE_STD)(gt.float())

utils/findstats.py
@@ -1,6 +1,7 @@
 import numpy as np
 import glob
 import os
+import tqdm
 
 
 def findFile(root_dir, endswith):
@@ -24,9 +25,12 @@ def findstats(path, endswith):
 
     image_list = []
     for image_path in path_list:
+        if "gt" in endswith:
+            image_list.extend(np.load(image_path).flatten())
+            continue
         image_list.append(np.load(image_path))
 
-    image_list = np.asarray(image_list)
+    image_list = np.asarray(image_list).flatten()
 
     mean = np.mean(image_list)
     std_dev = np.std(image_list)
@@ -40,12 +44,12 @@ def findstats(path, endswith):
 
 if __name__ == "__main__":
     # for the cal256 dataset, which only has amp and phase images
-    findstats('../data/arrays/train', 'amp_img.npy')
-    findstats('../data/arrays/train', 'phase_img.npy')
-
-    # for the vlbi online dataset, which has amp and phase images and labels
-    findstats('../data/arrays/val', 'amp_img.npy')
-    findstats('../data/arrays/val', 'amp_label.npy')
+    # findstats('../data/arrays/train', 'amp_img.npy')
+    # findstats('../data/arrays/train', 'phase_img.npy')
+    #
+    # # for the vlbi online dataset, which has amp and phase images and labels
+    # findstats('../data/arrays/val', 'amp_img.npy')
+    # findstats('../data/arrays/val', 'amp_gt.npy')
     findstats('../data/arrays/val', 'phase_img.npy')
-    findstats('../data/arrays/val', 'phase_gt.npy')
+    # findstats('../data/arrays/val', 'phase_gt.npy')
 

utils/preprocessing.py
@@ -4,6 +4,7 @@ from astropy.io import fits
 from myargs import args
 import numpy as np
 from tqdm import tqdm
+import math
 
 '''
     make some open all files with oifits ending and find their corresponding image, resize to 64x64, 
@@ -48,12 +49,12 @@ def process_datafolder(path):
             vcoords.append(visdata.vcoord)
             ucoords.append(visdata.ucoord)
             visibilities.append(visdata._visamp[0])
-            phases.append(visdata._visphi[0])
+            phases.append(visdata._visphi[0] / 180 * math.pi)
 
             vcoords.append(-visdata.vcoord)
             ucoords.append(-visdata.ucoord)
             visibilities.append(visdata._visamp[0])
-            phases.append(-visdata._visphi[0])
+            phases.append(-visdata._visphi[0] / 180 * math.pi)
 
         image = fits.open(gt_path)[0].data
         fft_image = np.fft.fft2(image)